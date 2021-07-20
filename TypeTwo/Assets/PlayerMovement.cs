672ac2c6083ea9f0d49cf534586e572e24cdd0a5
Micheal Quentin <mquenti3@gmail.com>
2021-07-20 17:33:22 -0500

    SetUp of Basic Scene

---
+++ TypeTwo/Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Animator animator;
+
+    public float speed = 6.0f;
+
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            animator.SetBool("isRunning", true);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
---
+++ TypeTwo/Assets/Resources/Scripts/GravityOrbit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityOrbit : MonoBehaviour
+{
+    public float gravity;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<GravityControl>())
+        {
+            other.GetComponent<GravityControl>().gravity = this.GetComponent<GravityOrbit>();
+        }
+    }
+}
